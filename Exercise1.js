5e4912023b233f01d7be2a46a885e5b87199acd7
Josh Vandeleuv <j741v016@ku.edu>
2019-03-08 11:41:53 -0600

    Finished Exercise 1

--- Exercise1.js
+++ Exercise1.js
@@ -4,14 +4,15 @@ function verifyPass()
   let password2 = document.getElementById("passTwo").value;
   if(password1 != password2)
   {
-    console.log("Passwords do not match");
+    document.getElementById("Status Text").innerHTML = "Passwords do not match";
+
   }
   else if (password1.length < 8)
   {
-    console.log("Your password must be at least 8 characters");
+    document.getElementById("Status Text").innerHTML = "Your password must be at least 8 characters";
   }
   else
   {
-    console.log("Password Accepted!");
+    document.getElementById("Status Text").innerHTML = "Password Accepted!";
   }
 }
